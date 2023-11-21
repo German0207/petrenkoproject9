361720ebe5910b84b8e18f7fb1a98174ce45b4df
German0207 <151635068+German0207@users.noreply.github.com>
2023-11-21 15:13:47 +0300

    Add files via upload

---
+++ petrenko/petrenko/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbdullaevJoskiPraktika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+          
+                double sum; // сумма
+                int period; // срок
+                double percent; // процентная ставка
+                double profit; // доход
+                sum = System.Convert.ToDouble(textBox1.Text);
+                period = System.Convert.ToInt32(textBox2.Text);
+                if (sum < 10000)
+                    percent = 3.5;
+                else
+                    percent = 4.5;
+                profit = sum * (percent / 100 / 12) * period;
+                label3.Text =
+                "Процентная ставка: " + percent.ToString("n") + "%\n" +
+                "Доход: " + profit.ToString("c");
+            
+
+        }
+    }
+}
---
+++ petrenko/petrenko/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("AbdullaevJoskiPraktika")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AbdullaevJoskiPraktika")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a620f1a1-b0fe-4d2e-a436-79e426c4b87e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
